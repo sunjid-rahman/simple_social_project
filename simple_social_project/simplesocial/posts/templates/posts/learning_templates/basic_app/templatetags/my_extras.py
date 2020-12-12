94883b1a3af437de32ffabbfe0b048e2309cfb4d
unknown <msun162073@bscse.uiu.ac.bd>
2020-12-12 15:52:16 +0600

    first

---
+++ simple_social_project/simplesocial/posts/templates/posts/learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,9 @@
+from django import template
+register=template.Library()
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+    This Cuts out all values of "arg" from the string!
+    """
+    return value.replace(arg,'')
+# register.filter('cut',cut)
